ef4fc84452ab1bee377687fe62603e19c68a99ac
Trifftnix <82308833+Trifftnix@users.noreply.github.com>
2021-04-13 00:44:45 +0200

    new folders

--- deployment/rezeptcreator.php
+++ deployment/rezeptcreator.php
@@ -1,21 +1,12 @@
 <?php
-/*$teststring = "Dies ist ein Testsatz!";
-$file = fopen('Textdateien/test.txt', 'a');
-ob_start();
-echo 
-fwrite ($file, $teststring);
-	fclose($file);
-*/
-	
-	
 
-$neuesRezept = "neuesRezept.json";
+$neuesRezept = $argv[1];
 $neueVariablen = file_get_contents($neuesRezept);
 $neueVariablen= json_decode ($neueVariablen);
 var_dump($neueVariablen);
 //Variablen
 $Rezeptname = $neueVariablen->Rezeptname;
-$BildnamemitEndung 	= $neueVariablen->BildnamemitEndung;
+$BildnameohneEndung 	= $neueVariablen->BildnameohneEndung;
 $Zeit 		= $neueVariablen->Zeit;
 //Schwierigkeit ist noch nicht umgesetzt
 $Portionen 	= $neueVariablen->Portionen;
@@ -36,7 +27,7 @@ foreach($neueVariablen->Zubereitungsschritte as $item) {
 ob_start();
 include 'recipeTemplate.php';
 $content = ob_get_contents();
-$template=  "recipe".$Rezeptname .".html";
+$template=  "recipes/recipe".$BildnameohneEndung.".html";
 file_put_contents ($template, $content);
 
 ob_end_clean();	
