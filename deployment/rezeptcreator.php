23624466c808e9c351d15e051613ebc142a038a6
Trifftnix <82308833+Trifftnix@users.noreply.github.com>
2021-04-14 17:11:20 +0200

    Update rezeptcreator.php

--- deployment/rezeptcreator.php
+++ deployment/rezeptcreator.php
@@ -16,17 +16,17 @@ $Portionen = $Schwierigkeit;
 $Sterne = "";
 
 //alle vollen Sterne
-for (i = $Schwierigkeit; i>=1; i--) {
+for ($i = $Schwierigkeit; $i>=1; $i--) {
 	$Sterne = $Sterne."<i class='fa fa-star' aria-hidden='true'></i>/n";
 } 
 
 //gibt es einen halben Stern?
-if (i == .5) {
+if ($i == .5) {
 	$Sterne = $Sterne."<i class='fa fa-star-half-o' aria-hidden='true'></i>/n";
 }
 
 //leere Sterne auffüllen
-for (i = (3-$Schwierigkeit); i; i--) {
+for ($i = (3-$Schwierigkeit); $i; $i--) {
 	$Sterne = $Sterne."<i class='fa fa-star-o' aria-hidden='true'></i>/n";
 }
 
