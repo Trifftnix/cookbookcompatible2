242357ed797771469c89af8552e79b962c219e67
Trifftnix <82308833+Trifftnix@users.noreply.github.com>
2021-04-20 13:17:51 +0200

    newrecipenotconsidered

--- deployment/allrecipesintohtml.php
+++ deployment/allrecipesintohtml.php
@@ -7,58 +7,59 @@ $allrecipes= file_get_contents("deployment/allrecipes.html");
 $neuesRezept = $argv[1];
 $neueVariablen = file_get_contents($neuesRezept);
 $neueVariablen= json_decode ($neueVariablen);
-var_dump($neueVariablen);
+//var_dump($neueVariablen);
 //Variablen
 $Rezeptname = $neueVariablen->Rezeptname;
-$BildnameohneEndung 	= $neueVariablen->BildnameohneEndung;
+if $Rezeptname != "" {
+	$BildnameohneEndung 	= $neueVariablen->BildnameohneEndung;
 
 
-$neueRezeptzeile = 
-	   "<div class='col-lg-4 col-md-6 col-sm-12 wow fadeIn'>
-          <div class='recipe-item text-center'>
-            <a href='recipes/recipe".$BildnameohneEndung.".html'>
-              <img src='recipes/images/".$BildnameohneEndung."resized.jpg' alt='".$Rezeptname."' />
-            </a>
-            <br />
-            <h3>".$Rezeptname."</h3>
-          </div>
-        </div>";
+	$neueRezeptzeile = 
+		   "<div class='col-lg-4 col-md-6 col-sm-12 wow fadeIn'>
+			  <div class='recipe-item text-center'>
+				<a href='recipes/recipe".$BildnameohneEndung.".html'>
+				  <img src='recipes/images/".$BildnameohneEndung."resized.jpg' alt='".$Rezeptname."' />
+				</a>
+				<br />
+				<h3>".$Rezeptname."</h3>
+			  </div>
+			</div>";
 
 
-$allrecipes= $allrecipes.$neueRezeptzeile;
+	$allrecipes= $allrecipes.$neueRezeptzeile;
 
 
-file_put_contents ("deployment/allrecipes.html", $allrecipes);
+	file_put_contents ("deployment/allrecipes.html", $allrecipes);
 
-if ($neueVariablen->backen == "ja") {
-	$backenrecipes= file_get_contents("deployment/backenrecipes.html");
-	$backenrecipes= $backenrecipes.$neueRezeptzeile;
-	file_put_contents ("deployment/backenrecipes.html", $backenrecipes);
+	if ($neueVariablen->backen == "ja") {
+		$backenrecipes= file_get_contents("deployment/backenrecipes.html");
+		$backenrecipes= $backenrecipes.$neueRezeptzeile;
+		file_put_contents ("deployment/backenrecipes.html", $backenrecipes);
+	}
+	if ($neueVariablen->fleisch == "ja") {
+		$fleischrecipes= file_get_contents("deployment/fleischrecipes.html");
+		$fleischrecipes= $fleischrecipes.$neueRezeptzeile;
+		file_put_contents ("deployment/fleischrecipes.html", $fleischrecipes);
+	}
+	if ($neueVariablen->fisch == "ja") {
+		$fischrecipes= file_get_contents("deployment/fischrecipes.html");
+		$fischrecipes= $fischrecipes.$neueRezeptzeile;
+		file_put_contents ("deployment/fischrecipes.html", $fischrecipes);
+	}
+	if ($neueVariablen->snacks == "ja") {
+		$snacksrecipes= file_get_contents("deployment/snacksrecipes.html");
+		$snacksrecipes= $snacksrecipes.$neueRezeptzeile;
+		file_put_contents ("deployment/snacksrecipes.html", $snacksrecipes);
+	}
+	if ($neueVariablen->spicy == "ja") {
+		$spicyrecipes= file_get_contents("deployment/spicyrecipes.html");
+		$spicyrecipes= $spicyrecipes.$neueRezeptzeile;
+		file_put_contents ("deployment/spicyrecipes.html", $spicyrecipes);
+	}
+	if ($neueVariablen->vegetarisch == "ja") {
+		$vegetarischrecipes= file_get_contents("deployment/vegetarischrecipes.html");
+		$vegetarischrecipes= $vegetarischrecipes.$neueRezeptzeile;
+		file_put_contents ("deployment/vegetarischrecipes.html", $vegetarischrecipes);
+	}
 }
-if ($neueVariablen->fleisch == "ja") {
-	$fleischrecipes= file_get_contents("deployment/fleischrecipes.html");
-	$fleischrecipes= $fleischrecipes.$neueRezeptzeile;
-	file_put_contents ("deployment/fleischrecipes.html", $fleischrecipes);
-}
-if ($neueVariablen->fisch == "ja") {
-	$fischrecipes= file_get_contents("deployment/fischrecipes.html");
-	$fischrecipes= $fischrecipes.$neueRezeptzeile;
-	file_put_contents ("deployment/fischrecipes.html", $fischrecipes);
-}
-if ($neueVariablen->snacks == "ja") {
-	$snacksrecipes= file_get_contents("deployment/snacksrecipes.html");
-	$snacksrecipes= $snacksrecipes.$neueRezeptzeile;
-	file_put_contents ("deployment/snacksrecipes.html", $snacksrecipes);
-}
-if ($neueVariablen->spicy == "ja") {
-	$spicyrecipes= file_get_contents("deployment/spicyrecipes.html");
-	$spicyrecipes= $spicyrecipes.$neueRezeptzeile;
-	file_put_contents ("deployment/spicyrecipes.html", $spicyrecipes);
-}
-if ($neueVariablen->vegetarisch == "ja") {
-	$vegetarischrecipes= file_get_contents("deployment/vegetarischrecipes.html");
-	$vegetarischrecipes= $vegetarischrecipes.$neueRezeptzeile;
-	file_put_contents ("deployment/vegetarischrecipes.html", $vegetarischrecipes);
-}
-
 ?>
--- deployment/rezeptcreator.php
+++ deployment/rezeptcreator.php
@@ -3,53 +3,54 @@
 $neuesRezept = $argv[1];
 $neueVariablen = file_get_contents($neuesRezept);
 $neueVariablen= json_decode ($neueVariablen);
-var_dump($neueVariablen);
+//var_dump($neueVariablen);
 //Variablen
 $Rezeptname = $neueVariablen->Rezeptname;
-$BildnameohneEndung 	= $neueVariablen->BildnameohneEndung;
-$Zeit 		= $neueVariablen->Zeit;
-$Schwierigkeit = $neueVariablen->Schwierigkeit;
-$Schwierigkeit = str_replace (",", ".", $Schwierigkeit);
-$Schwierigkeit = round($Schwierigkeit*2)/2;
+if $Rezeptname != "" {
+	$BildnameohneEndung 	= $neueVariablen->BildnameohneEndung;
+	$Zeit 		= $neueVariablen->Zeit;
+	$Schwierigkeit = $neueVariablen->Schwierigkeit;
+	$Schwierigkeit = str_replace (",", ".", $Schwierigkeit);
+	$Schwierigkeit = round($Schwierigkeit*2)/2;
 
-$Sterne = "";
+	$Sterne = "";
 
-//alle vollen Sterne
-for ($i = $Schwierigkeit; $i>=1; $i--) {
-	$Sterne = $Sterne."<i class='fa fa-star' aria-hidden='true'></i>\n";
-} 
+	//alle vollen Sterne
+	for ($i = $Schwierigkeit; $i>=1; $i--) {
+		$Sterne = $Sterne."<i class='fa fa-star' aria-hidden='true'></i>\n";
+	} 
 
-//gibt es einen halben Stern?
-if ($i == .5) {
-	$Sterne = $Sterne."<i class='fa fa-star-half-o' aria-hidden='true'></i>\n";
-}
+	//gibt es einen halben Stern?
+	if ($i == .5) {
+		$Sterne = $Sterne."<i class='fa fa-star-half-o' aria-hidden='true'></i>\n";
+	}
 
-//leere Sterne auffüllen
-for ($i = (3-$Schwierigkeit); $i>=1; $i--) {
-	$Sterne = $Sterne."<i class='fa fa-star-o' aria-hidden='true'></i>\n";
-}
+	//leere Sterne auffüllen
+	for ($i = (3-$Schwierigkeit); $i>=1; $i--) {
+		$Sterne = $Sterne."<i class='fa fa-star-o' aria-hidden='true'></i>\n";
+	}
 
-$Portionen 	= $neueVariablen->Portionen;
-$Zutaten = "\t";
-foreach($neueVariablen->ZutatenEinkaufsliste as $item) {
-	$Zutaten = $Zutaten."<div itemprop='ingredients'><dt>".$item->Menge."</dt><dd>".$item->Zutat."</dd></div>\n\t\t\t\t";
-};
-$Zutaten2 = "\t";
-foreach($neueVariablen->Zutatenschonvorhanden as $item) {
-	$Zutaten2 = $Zutaten2."<dt>".$item->Menge."</dt>	<dd>".$item->Zutat."</dd>\n\t\t\t\t";
-};
-$Zubereitungsschritte ="";
-foreach($neueVariablen->Zubereitungsschritte as $item) {
-	$Zubereitungsschritte = $Zubereitungsschritte."<li>".$item."</li>\n\t\t\t\t";
-};
+	$Portionen 	= $neueVariablen->Portionen;
+	$Zutaten = "\t";
+	foreach($neueVariablen->ZutatenEinkaufsliste as $item) {
+		$Zutaten = $Zutaten."<div itemprop='ingredients'><dt>".$item->Menge."</dt><dd>".$item->Zutat."</dd></div>\n\t\t\t\t";
+	};
+	$Zutaten2 = "\t";
+	foreach($neueVariablen->Zutatenschonvorhanden as $item) {
+		$Zutaten2 = $Zutaten2."<dt>".$item->Menge."</dt>	<dd>".$item->Zutat."</dd>\n\t\t\t\t";
+	};
+	$Zubereitungsschritte ="";
+	foreach($neueVariablen->Zubereitungsschritte as $item) {
+		$Zubereitungsschritte = $Zubereitungsschritte."<li>".$item."</li>\n\t\t\t\t";
+	};
 
 
-ob_start();
-include 'deployment/recipeTemplate.php';
-$content = ob_get_contents();
-$template=  "recipes/recipe".$BildnameohneEndung.".html";
-file_put_contents ($template, $content);
+	ob_start();
+	include 'deployment/recipeTemplate.php';
+	$content = ob_get_contents();
+	$template=  "recipes/recipe".$BildnameohneEndung.".html";
+	file_put_contents ($template, $content);
 
-ob_end_clean();	
-	
+	ob_end_clean();	
+}	
 ?>
